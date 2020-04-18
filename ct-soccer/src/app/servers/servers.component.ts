43aa7bbb6058c67685388238420d61d1b180e965
chylinski.t <chylinski.t@outlook.com>
2020-04-17 21:00:59 -0400

    feat(ct-soccer): event binding, data binding, two way binding

--- ct-soccer/src/app/servers/servers.component.ts
+++ ct-soccer/src/app/servers/servers.component.ts
@@ -3,13 +3,47 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'Testserver';
+  username = '';
+  allowNewUser = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
   }
 
+  ngOnInit(): void {}
+
+  onCreateServer() {
+    this.serverCreationStatus = `Server was Created name is ${this.serverName}`;
+  }
+
+  onUpdateServerNmae(event: any) {
+    this.serverName = event.target.value;
+  }
+
+  updateUserBtn(event: any) {
+    this.username = event.target.value;
+    if (this.username.length >= 1) {
+      this.allowNewUser = true;
+    }else{
+      this.allowNewUser = false;
+    }
+  }
+
+  enableUser() {
+    if (this.username.length < 1) {
+      this.allowNewUser = true;
+    }
+  }
+
+  resetUser() {
+    this.username = '';
+  }
 }
