7f6195ecb2c0e3a336fea647e071a70699a4e09e
chylinski.t <chylinski.t@outlook.com>
2020-06-12 23:23:50 -0400

    play component added/routing

--- ct-soccer/src/app/app.module.ts
+++ ct-soccer/src/app/app.module.ts
@@ -15,10 +15,12 @@ import { NewsfeedComponent } from './homepage/newsfeed/newsfeed.component';
 import { FeedComponent } from './play/feed/feed.component';
 import { EditPostsComponent } from './play/feed/edit-posts/edit-posts.component';
 import { Routes, RouterModule } from '@angular/router';
+import { PlayComponent } from './play/play/play.component';
 
 const appRoutes: Routes = [
 {path: '', component: HomepageComponent},
 {path: 'home', component: HomepageComponent},
+,{path: 'play', component: PlayComponent},
 {path: 'news', component: NewsfeedComponent},
 {path: 'shop', component: StoreComponent},
 {path: 'schedule', component: ScheduleComponent},
@@ -38,7 +40,8 @@ const appRoutes: Routes = [
     StoreComponent,
     NewsfeedComponent,
     FeedComponent,
-    EditPostsComponent
+    EditPostsComponent,
+    PlayComponent
   ],
   imports: [
     BrowserModule,
